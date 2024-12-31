2df6fb74bbb10d39fd8c4332370929cdb2a8a0fc
unknownss-ss <sk220028a@nulondonstudents.org>
2024-12-31 14:25:37 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 
+function converting() {
+  const value = parseFloat(document.getElementById('input-box').value);
+  const unit = document.getElementById('select-size').value;
+  const result = document.getElementById('select-size-output').value;
+
+
+const convertingby ={
+  byes:1,
+  kilobytes: 1024,
+  megabytes: 1024 * 1024,
+  gigabytes: 1024 * 1024 * 1024,
+
+};
+
+const bytesconversion = value *convertingby[unit];
+const convertedvalue = bytesconversion / convertingby[result];
+
+document.getElementById('output-box').value = convertedvalue; //.toFixed(2) for rounding to 2dp
+                                       
+}
+
+document.getElementById('convert-button').addEventListener('click', converting);
